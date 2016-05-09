5d00fd8c2658b13ba232964e14d3822010f20956
yonelyvan <yonelyvan@gmail.com>
2016-05-09 16:34:27 -0500

    Update comandos-git.sql

--- comandos-git.sql
+++ comandos-git.sql
@@ -7,7 +7,8 @@ git commit -m "Commit message"
 git push origin master 	--envio de cambios
 --------------------------------------
 git pull 	--actulaizar ultimo commit 
-====================================
+==============configuacion==============
 --mas ocmandos
 git config --global user.email "yonelyvan@gmail.com"
 git config --global user.name "yonelyvan"
+===================================
